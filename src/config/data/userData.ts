7ffcd70b7c95927c40938cea4c31dc93a6454cfe
Shen GuoQing <yinweiyingzhe@126.com>
2024-01-18 11:46:20 +0800

    优化请求

--- src/config/data/userData.ts
+++ src/config/data/userData.ts
@@ -1,14 +1,18 @@
 import { BaseData } from "complex-data";
 import { BaseDataInitOption } from "complex-data/src/data/BaseData";
-import loginApi, { loginApiArg } from "@/api/main/loginApi";
+import loginApi, { loginApiArg, userInfo } from "@/api/main/loginApi";
 
 export class UserData extends BaseData{
+  data: Partial<userInfo>
   constructor(initOption: BaseDataInitOption) {
     super(initOption)
+    this.data = {}
   }
   login(data: loginApiArg) {
     return new Promise((resolve, reject) => {
       loginApi.require(data).then(res => {
+        console.log(res)
+        this.data = res.data
         resolve(res)
       }).catch(err => {
         reject(err)
