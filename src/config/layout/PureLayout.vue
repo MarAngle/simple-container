147c32d8b6aace98a69ebb995f48793fc41c93b0
Shen GuoQing <yinweiyingzhe@126.com>
2024-01-19 16:20:30 +0800

    添加样式管理

---
+++ src/config/layout/PureLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <keep-alive v-if="$route.meta.keepAlive">
+      <component :is="Component" />
+    </keep-alive>
+    <component v-else :is="Component" />
+  </router-view>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PureLayout'
+})
+</script>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,18 @@
 
 <style scoped>
 .login-index {
-
+  .login-test{
+    width: 50px;
+    height: 50px;
+    background-color: aqua;
+  }
 }
 
 </style>
 
 <template>
   <div class="login-index">
+    <div class="login-test"></div>
     <a-button @click="onLogin">登录</a-button>
   </div>
 </template>
